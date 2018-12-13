72568daddc158a60076c31179567f8a717aabeb8
edwinachan <edwina.chan@bbc.co.uk>
2018-12-13 20:40:03 +0000

    Update codewars-kata10-find-unique-letter.py

--- codewars-kata10-find-unique-letter.py
+++ codewars-kata10-find-unique-letter.py
@@ -1,3 +1,5 @@
+#Find the missing letter
+
 #Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
 
 #You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
