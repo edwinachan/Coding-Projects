bde94719cb181e59d989e07d4e957e3e772d9bdc
edwinachan <edwina.chan@bbc.co.uk>
2018-12-13 17:19:37 +0000

    Create codewars-kata09-find-unique-number.py

---
+++ codewars-kata09-find-unique-number.py
@@ -0,0 +1,14 @@
+#There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+#findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+#findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+#It’s guaranteed that array contains more than 3 numbers.
+
+#The tests contain some very huge arrays, so think about performance.
+
+def find_uniq(arr):
+    arr.sort()
+    if arr[0] == arr[1]:
+        return arr[len(arr)-1]
+    elif arr[0] != arr[1]:
+        return arr[0]
