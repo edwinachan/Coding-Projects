d91bd398fc4c97c40ea75d1b919f838cb4ff16ec
edwinachan <edwina.chan@bbc.co.uk>
2018-12-14 09:55:32 +0000

    Update codewars-kata10-find-unique-letter.py

--- codewars-kata10-find-unique-letter.py
+++ codewars-kata10-find-unique-letter.py
@@ -13,20 +13,33 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def find_missing_letter(chars):
-    lowerCase = [x.lower() for x in chars] #Use list comprehension to convert to lower case
 
+    lowerCase = [x.lower() for x in chars] #Use list comprehension to convert to lower case
+    
     lower = lowerCase[0] #define the 'smallest' letter
     lowerInAlpha = alphabet.index(lower) #this gives the position of the smallest letter in the alphabet
     upper = lowerCase[-1] #define the 'largest' letter
     upperInAlpha = alphabet.index(upper) #this gives the position of the largest letter in the alphabet
     #We now have the lower and upper bound of the range of chars
 
-
     for i in range(lowerInAlpha, upperInAlpha+1): #We must use upperinAlpha + 1 as the range(start,stop) function generates numbers up to but not including the figure which represents 'stop' 
         if alphabet[i] not in lowerCase:
-            return alphabet[i]
-#Why is this for loop incorrect when I use "not in chars"!?
- 
+                if chars[0].istitle(): #Check the case of the given list
+                    return alphabet[i].upper() #Convert to original case
+                else:
+                    return alphabet[i]
+        
 chars = ['O','Q','R','S']
+chars1 = ['a','b','c','d','f']
 
-print(find_missing_letter(chars))
+print(find_missing_letter(chars1))
+
+#A more elegant solution:
+
+def find_missing_letter(input):
+    alphabet = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    start = alphabet.index(input[0])
+    for i in range(len(input)):
+        if not input[i] == alphabet[start+i]:
+            return alphabet[start+i]
+
