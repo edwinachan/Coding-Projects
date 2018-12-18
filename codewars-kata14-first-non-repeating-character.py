c48aa767b1c63256f22f3368c9a1db708e5bed82
edwinachan <edwina.chan@bbc.co.uk>
2018-12-18 21:10:48 +0000

    Update codewars-kata14-first-non-repeating-character.py

--- codewars-kata14-first-non-repeating-character.py
+++ codewars-kata14-first-non-repeating-character.py
@@ -1,3 +1,5 @@
+#First non-repeating character
+
 #Write a function named firstNonRepeatingLetter† that takes a string input, and returns the first character that is not repeated anywhere in the string.
 
 #For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
