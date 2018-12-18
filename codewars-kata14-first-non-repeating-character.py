546fa74eaaf018130f931f2c9b5e49f0b4b75d14
edwinachan <edwina.chan@bbc.co.uk>
2018-12-18 21:10:04 +0000

    Update codewars-kata14-first-non-repeating-character.py

--- codewars-kata14-first-non-repeating-character.py
+++ codewars-kata14-first-non-repeating-character.py
@@ -8,6 +8,8 @@
 
 def first_non_repeating_letter(string):
 
+    count = 0
+
     stringLower = string.lower()
 
     if stringLower == '':
@@ -16,3 +18,32 @@ def first_non_repeating_letter(string):
     for i in range(len(string)):
         if stringLower.count(stringLower[i]) == 1:
             return string[i]
+
+    for i in range(len(string)):
+        if stringLower.count(stringLower[i]) == 2:
+            count += 1
+            if count == ((len(stringLower)+1)//2):
+                return ''
+            
+  
+#Test cases
+#Test.describe('Basic Tests')
+#Test.it('should handle simple tests')
+#Test.assert_equals(first_non_repeating_letter('a'), 'a')
+#Test.assert_equals(first_non_repeating_letter('stress'), 't')
+#Test.assert_equals(first_non_repeating_letter('moonmen'), 'e')
+
+#Test.it('should handle empty strings')
+#Test.assert_equals(first_non_repeating_letter(''), '')
+
+#Test.it('should handle all repeating strings') 
+#Test.assert_equals(first_non_repeating_letter('abba'), '')
+#Test.assert_equals(first_non_repeating_letter('aa'), '')
+
+#Test.it('should handle odd characters')
+#Test.assert_equals(first_non_repeating_letter('~><#~><'), '#')
+#Test.assert_equals(first_non_repeating_letter('hello world, eh?'), 'w')
+
+#Test.it('should handle letter cases')
+#Test.assert_equals(first_non_repeating_letter('sTreSS'), 'T')
+#Test.assert_equals(first_non_repeating_letter('Go hang a salami, I\'m a lasagna hog!'), ',')
