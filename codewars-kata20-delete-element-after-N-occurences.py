53aaa1c686afb0b33740e979a1e1d5373006c58e
edwinachan <edwina.chan@bbc.co.uk>
2019-01-07 13:13:44 +0000

    Create codewars-kata20-delete-element-after-N-occurences.py

---
+++ codewars-kata20-delete-element-after-N-occurences.py
@@ -0,0 +1,20 @@
+#Delete occurrences of an element if it occurs more than n times
+
+#Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering.
+#For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+
+#delete_nth ([1,1,1,1],2) # return [1,1]
+
+#delete_nth ([20,37,20,21],1) # return [20,37,21]
+
+#Test.assert_equals(delete_nth([20,37,20,21], 1), [20,37,21])
+#Test.assert_equals(delete_nth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
+
+def delete_nth(order,max_e):
+    newList = []
+    
+    for i in order:
+        if newList.count(i) < max_e:
+            newList.append(i)
+    return newList
+
